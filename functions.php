ee975a4af50e83dc9aa93c7b977bdc6f52a65243
Niklas <nb@niklasbarning.de>
2018-06-29 14:50:04 +0200

    ACF Hotfixes

--- functions.php
+++ functions.php
@@ -226,11 +226,72 @@ if(function_exists("register_field_group"))
 		),
 		'menu_order' => 3,
 	));
-}
 
-if( function_exists('acf_add_local_field_group') ):
+	register_field_group(array (
+		'key' => 'group_5b3628dc83878',
+		'title' => 'Sponsoring Info',
+		'fields' => array(
+			array(
+				'key' => 'field_5b3628de24fcd',
+				'label' => 'Sponsoring Headline',
+				'name' => 'sponsoring_headline',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '50',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+			array(
+				'key' => 'field_5b3628e824fce',
+				'label' => 'Sponsoring Subheadline',
+				'name' => 'sponsoring_subheadline',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '50',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-templates/front.php',
+				),
+			),
+		),
+		'menu_order' => 4,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => array(
+			0 => 'the_content',
+		),
+		'active' => 1,
+		'description' => '',
+	));
 
-	acf_add_local_field_group(array(
+	register_field_group(array (
 		'key' => 'group_5b3619bcda579',
 		'title' => 'Sponsoring',
 		'fields' => array(
@@ -305,8 +366,7 @@ if( function_exists('acf_add_local_field_group') ):
 				),
 			),
 		),
-		'menu_order' => 0,
-		'position' => 'normal',
+		'menu_order' => 5,
 		'style' => 'default',
 		'label_placement' => 'top',
 		'instruction_placement' => 'label',
@@ -314,5 +374,4 @@ if( function_exists('acf_add_local_field_group') ):
 		'active' => 1,
 		'description' => '',
 	));
-	
-	endif;
+}
